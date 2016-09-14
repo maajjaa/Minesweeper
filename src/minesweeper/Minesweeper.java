46db0df64b44908be7be8969f4e96030b31927e7
Maja <Maja@Maja-PC>
2016-09-14 13:32:45 +0200

    Frame and panel

---
+++ src/minesweeper/Minesweeper.java
@@ -0,0 +1,15 @@
+package minesweeper;
+
+public class Minesweeper {
+
+    public static void main(String[] args) {
+      
+        MineFrame frame = new MineFrame();
+        
+        frame.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+        
+         
+    }
+    
+}
